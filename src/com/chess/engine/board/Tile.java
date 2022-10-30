73c9b3c93766ca3a2ec7891198018ec97e4f9c28
BorisGM <93388190+BorisGM@users.noreply.github.com>
2022-10-30 21:47:42 +0200

    ch

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -2,12 +2,32 @@ package com.chess.engine.board;
 
 
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Tile {
 
-    int tileCoordinate;
+   protected final int tileCoordinate;
 
-    public Tile(int tileCoordinate) {
+   private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+
+        final  Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for (int i = 0; i < 64; i++) {
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece){
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+    }
+    private Tile(int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
     }
 
@@ -16,7 +36,7 @@ public abstract class Tile {
     public  abstract Piece getPiece();
 
     public  static final class EmptyTile extends Tile{
-        public EmptyTile(int coordinate) {
+        public EmptyTile(final int coordinate) {
            super(coordinate);
         }
         @Override
@@ -31,7 +51,7 @@ public abstract class Tile {
     }
     public static final class OccupiedTile extends Tile{
 
-        Piece pieceOnTile;
+       private final Piece pieceOnTile;
 
         OccupiedTile(int tileCoordinate, Piece pieceOnTile){
             super(tileCoordinate);
